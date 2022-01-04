3ae041b2b46e8cb0bd120f130d4b05076a905a9b
riduan <mohammadriduan39@gmail.com>
2022-01-04 14:18:09 +0600

    launcher

---
+++ lib/url launcher.dart
@@ -0,0 +1,166 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class urllauncher extends StatefulWidget {
+  const urllauncher({Key? key}) : super(key: key);
+
+  @override
+  _urllauncherState createState() => _urllauncherState();
+}
+
+class _urllauncherState extends State<urllauncher> {
+  bool _hasCallSupport = false;
+  Future<void>? _launched;
+  String _phone = '';
+  @override
+  void initState() {
+    super.initState();
+    // Check for phone call support.
+    canLaunch('tel:123').then((bool result) {
+      setState(() {
+        _hasCallSupport = result;
+      });
+    });
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: false,
+      forceWebView: false,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchInWebViewOrVC(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: true,
+      forceWebView: true,
+      headers: <String, String>{'my_header_key': 'my_header_value'},
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchInWebViewWithJavaScript(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: true,
+      forceWebView: true,
+      enableJavaScript: true,
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchInWebViewWithDomStorage(String url) async {
+    if (!await launch(
+      url,
+      forceSafariVC: true,
+      forceWebView: true,
+      enableDomStorage: true,
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchUniversalLinkIos(String url) async {
+    final bool nativeAppLaunchSucceeded = await launch(
+      url,
+      forceSafariVC: false,
+      universalLinksOnly: true,
+    );
+    if (!nativeAppLaunchSucceeded) {
+      await launch(
+        url,
+        forceSafariVC: true,
+      );
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launch(launchUri.toString());
+    }
+    String toLaunch = 'https://www.cylog.org/headers/';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Appbar"),
+            centerTitle: true,
+          ),
+          body: ListView(
+            children:[ Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                      onChanged: (String text) => _phone = text,
+                      decoration: const InputDecoration(
+                          hintText: 'Input the phone number to launch')),
+                ),
+                ElevatedButton(
+                  onPressed: _hasCallSupport
+                      ? () => setState(() {
+                    _launched = _makePhoneCall(_phone);
+                  })
+                      : null,
+                  child: _hasCallSupport
+                      ? const Text('Make phone call')
+                      : const Text('Calling not supported'),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    _launched = _launchInBrowser(toLaunch);
+                  }),
+                  child:Text('Launch in browser'),
+                ),
+                Padding(padding: EdgeInsets.all(16.0)),
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    _launched = _launchInWebViewOrVC(toLaunch);
+                  }),
+                  child:Text('Launch in app'),
+                ),
+
+                Link(
+                  uri: Uri.parse(
+                      'https://pub.dev/documentation/url_launcher/latest/link/link-library.html'),
+                  target: LinkTarget.blank,
+                  builder: (BuildContext ctx, FollowLink? openLink) {
+                    return TextButton.icon(
+                      onPressed: openLink,
+                      label: Text('Link Widget documentation'),
+                      icon: Icon(Icons.read_more),
+                    );
+                  },
+                ),
+                FutureBuilder<void>(future: _launched, builder: _launchStatus),
+              ],
+            ),],
+          ),
+        ));
+  }
+}
